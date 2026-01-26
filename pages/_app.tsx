3b0414d8a60af645b6300850b07e7f579670d96e
kinectacademy <olivertan@kinectacademy.org>
2026-01-26 20:04:36 +0800

    remove middleware and sync

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
-import '@/styles/globals.css'
-import { PlasmicRootProvider } from "@plasmicapp/react-web";
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import Link from "next/link";
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+  // We return the component directly without any Auth wrappers
   return (
-    <PlasmicRootProvider Head={Head} Link={Link}>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
-    </PlasmicRootProvider>
+    </>
   );
 }
+
+export default MyApp;
