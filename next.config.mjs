ee37905a68eedcc435c1a0ef34db4b9f9a5b46ba
kinectacademy <olivertan@kinectacademy.org>
2026-01-26 17:38:50 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // This enables the static export for Cloudflare
+  images: {
+    unoptimized: true, // Required for static export to work with images
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
