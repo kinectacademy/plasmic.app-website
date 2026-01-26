f05e60d01e8442bd9a431b6871bfc8f8c7c00ad5
kinectacademy <olivertan@kinectacademy.org>
2026-01-26 18:03:00 +0800

    GlobalContextWrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Link from "next/link";
+import GlobalContextWrapper from "../components/GlobalContextWrapper";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlasmicRootProvider Head={Head} Link={Link}>
-      <Component {...pageProps} />
+      <GlobalContextWrapper>
+        <Component {...pageProps} />
+      </GlobalContextWrapper>
     </PlasmicRootProvider>
   );
 }
