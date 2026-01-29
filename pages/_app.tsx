66639e995d31d728b06f733589217ac230591069
kinectacademy <olivertan@kinectacademy.org>
2026-01-29 13:25:41 +0800

    page animation fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,16 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { ReactLenis } from 'lenis/react';
+import { useEffect } from 'react'; // Added to debug
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
+
+  // Debugging: This will print in your browser console every time you change pages
+  useEffect(() => {
+    console.log("Route changed to:", router.asPath);
+  }, [router.asPath]);
 
   return (
     <ReactLenis root options={{ lerp: 0.1, duration: 1.2 }}>
@@ -13,8 +19,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       
-      {/* We use a simple div with a CSS class that restarts on route change */}
-      <div key={router.asPath} className="page-transition">
+      {/* Use a unique ID and key to ensure the browser sees this as a 'new' element on every page */}
+      <div 
+        id="page-wrapper"
+        key={router.asPath} 
+        className="page-fade-in"
+      >
         <Component {...pageProps} />
       </div>
     </ReactLenis>
